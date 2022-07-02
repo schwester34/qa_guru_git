d8e442117a6d0a6da34ef2f3a2a3e65165ebf199
schwester34 <yana64604@gmail.com>
2022-07-03 00:30:38 +0500

    hi

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -3,9 +3,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-        //hi
         System.out.println("Hello, QA.guru!");
-        System.out.println("Another method");
+        System.out.println("Another line");
+        System.out.println("slow branch");
+    }
 
+    public static void another(){
+        System.out.println("ANOTHER METHOD");
     }
 }
